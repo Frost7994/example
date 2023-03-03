e4938912504844bf54971b8b2faf16b595459c57
tommy byrne <tommybyrne9@gmail.com>
2023-03-03 17:23:05 +0000

    added basics for tailwind and next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+// components
+import { SessionProvider } from "next-auth/react";
+
 // styles
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />;
+        </SessionProvider>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+// utils
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Home = () => {
-    return <div>Home</div>;
+    // session destructure
+    let { data: session } = useSession();
+
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-medium">NextAuth Home</h1>
+            {session ? (
+                <>
+                    <p>You are logged in as: {session?.user?.email}</p>
+                    <button
+                        onClick={() => signOut()}
+                        className="text-sm font-medium text-sky-500 border-sky-500 border rounded-md px-3 py-2 transition-all hover:bg-sky-500/20">
+                        Sign Out
+                    </button>
+                </>
+            ) : (
+                <>
+                    <p className="mb-2 ">Please log in to see next auth in use.</p>
+                    <button
+                        onClick={() => signIn()}
+                        className="text-sm font-medium text-sky-500 border-sky-500 border rounded-md px-3 py-2 transition-all hover:bg-sky-500/20">
+                        Sign In
+                    </button>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Home;
